4b1c0862e13c13f1b4673ac18085a9d55916e936
Jihyeon <zz64446949@gmail.com>
2024-05-30 15:21:17 +0900

    파일 수정

---
+++ YeonBa/GlobalAPI/NotificationAPI/DTO/Response/NotificationAcceptResponse.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationAcceptResponse .swift
+//  YeonBa
+//
+//  Created by jin on 5/30/24.
+//
+
+import Foundation
+
+struct NotificationAcceptResponse: Codable {
+    let chatRoomId : Int?
+}
